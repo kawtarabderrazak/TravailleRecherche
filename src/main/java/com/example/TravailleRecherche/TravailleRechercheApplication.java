16d7d9f71bac0020f48a7d5a6d185cdbbcdcd3d6
kawtarabderrazak <kawtar.abderrazak1@gmail.com>
2024-12-16 21:43:28 +0100

    first try

---
+++ src/main/java/com/example/TravailleRecherche/TravailleRechercheApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravailleRecherche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravailleRechercheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravailleRechercheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TravailleRecherche/TravailleRechercheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravailleRecherche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravailleRechercheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
